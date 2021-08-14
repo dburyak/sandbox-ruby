1951ac084af65442d02b658771b7ac8522eaaea6
Dmytro Buryak <dmytro.buryak@gmail.com>
2021-08-14 18:54:07 +0300

    Added require_relative example

--- src/require_relative.rb
+++ src/require_relative.rb
@@ -1,17 +1,8 @@
 puts 'main file start, requiring print_hello .....'
-require './print_hello'
+require_relative 'print_hello'
 puts 'done requiring by main file'
 puts 'requiring 3 times more from the main:'
 3.times do
-  require './print_hello'
+  require_relative 'print_hello'
 end
 puts 'finished requiring 3 files'
-puts
-puts "loadpath:"
-$:.each do |it|
-  puts it
-end
-puts
-require 'scanf'
-parts = 'David Black'.scanf("%s%s")
-puts parts
